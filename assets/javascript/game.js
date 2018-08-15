37736f8877c393cca527271a8f3defdaa5b5a7a3
Thomas Martin <tmartin.dev101010@gmail.com>
2018-08-15 17:04:15 -0600

    added the character class in game.js.  Added the links for jQuery in index.html.

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Class for character
+class Character {
+    constructor(healthPts, attackPts, counterAttackPts ) {
+        this.healthPts = healthPts;
+        this.attackPts = attackPts;
+        this.counterAttackPts = counterAttackPts;
+    }
+}
+
+$(document).ready(function() {
+
+}); 
