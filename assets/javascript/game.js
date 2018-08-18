dda4ad933b79ae8a0f4acd532e669f681154aaee
Thomas Martin <tmartin.dev101010@gmail.com>
2018-08-18 08:12:31 -0600

    Working on the character tiles.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,44 @@
 // Class for character
 class Character {
-    constructor(healthPts, attackPts, counterAttackPts ) {
+    constructor(name, picture, charID, healthPts, attackPts, counterAttackPts ) {
+        this.name = name
+        this.picture = picture;
+        this.charID = charID;
         this.healthPts = healthPts;
         this.attackPts = attackPts;
         this.counterAttackPts = counterAttackPts;
     }
 }
 
+function buildCharacterTile(character) {
+    var newCharacter = $("<div>");
+    newCharacter.addClass("character-tile");
+
+    // Adding stuff to tile
+    var tempElement = $("<div>");
+    tempElement.text(character.name);
+    newCharacter.append(tempElement);
+    tempElement = $("<img>");
+    tempElement.attr("src", character.picture);
+    newCharacter.append(tempElement);
+
+    tempElement = $("<div>");
+    tempElement.add
+    tempElement.text(character.healthPts);
+    
+    $(".character-list").append(newCharacter);
+}
+
 $(document).ready(function() {
+
+    // Create a new character
+    var Vader = new Character("Darth Vader", "./assets/images/Vader-1.jpg" , 100, 25, 10);
+    console.log(Vader.name);
+    console.log(Vader.healthPts);
+    console.log(Vader.attackPts);
+    console.log(Vader.counterAttackPts);
+
+    buildCharacterTile(Vader);
+    
 
 }); 
