528a12f46d41bd8e1c9824afc6172faf4788a83f
Thomas Martin <tmartin.dev101010@gmail.com>
2018-08-21 05:36:57 -0600

    Logic around winning and losing in place.  Tiles are now updating.  Information is displaying on status.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,11 @@ class Character {
         this.attackPts = attackPts;
         this.counterAttackPts = counterAttackPts;
     }
-
-    attackEnemy() {
-        
-    }
 }
 
 var isPlayerSelected = false;
 var isEnemySelected = false;
+var currentAttackPts = 0;
 
 // Charater during gameplay
 var myCharacter = null;
@@ -35,19 +32,55 @@ function buildCharacterTile(character) {
     newCharacter.append(tempElement);
 
     tempElement = $("<div>");
+    tempElement.attr("id", "hitPoints");
     tempElement.text(character.healthPts);
     newCharacter.append(tempElement);
     
     $(".character-list").append(newCharacter);
 }
 
+function evaluateAttack() {
+    // User character will always win the tie.
+    myEnemy.healthPts -= currentAttackPts;
+
+    $("#status").empty();
+    $("#status").append("<p>" + myCharacter.name + " has attacked with " + currentAttackPts + "!</p>");
+    
+    // Check to see if the Enemy is dead
+    if (myEnemy.healthPts <= 0) {
+        // console.log("Enemy is dead...");
+        $("#status").append("<p>" + myEnemy.name + " was defeated! Pick another fighter! </p>");
+        // Do stuff to kill the enemy
+        $(".defender>.tile").fadeOut(1500);
+        
+        if ($(".enemy-list .tile").length > 0) {
+            isEnemySelected = false;
+        } else {
+            console.log("You won!");
+        }
+    } else {
+        // Update the enemy hitpoint display
+        $(".defender #hitPoints").text(myEnemy.healthPts);
+        
+        myCharacter.healthPts -= myEnemy.counterAttackPts;
+        $("#status").append("<p>" + myEnemy.name + " has counter attacked with " + myEnemy.counterAttackPts + "!</p>");
+
+        if (myCharacter.healthPts <= 0) {
+            console.log("Game Over.");
+        } else {
+            $("#my-character #hitPoints").text(myCharacter.healthPts);
+        }
+    }
+
+}
+
 $(document).ready(function() {
 
     // Create a new character
-    var characters = [ new Character("Darth Vader", "./assets/images/Vader-1.jpg", 0, 100, 25, 10),
-                       new Character("Luke Skywalker", "./assets/images/luke-1.jpg", 1, 80, 30, 110),
-                       new Character("Boba Fett", "./assets/images/boba-1.jpg", 2, 75, 20, 15),
-                       new Character("Yoda", "./assets/images/yoda-1.jpg", "yoda", 50, 10, 20)];
+    var characters = [ new Character("Darth Vader", "./assets/images/Vader-1.jpg", 0, 125, 40, 20),
+                       new Character("Luke Skywalker", "./assets/images/luke-1.jpg", 1, 100, 60, 30),
+                       new Character("Boba Fett", "./assets/images/boba-1.jpg", 2, 80, 80, 40),
+                       new Character("Yoda", "./assets/images/yoda-1.jpg", 3, 200, 20, 10)];
     
     for (let i = 0; i < characters.length; i++) {
         buildCharacterTile(characters[i]);
@@ -58,7 +91,7 @@ $(document).ready(function() {
         if (!isPlayerSelected) {
             var myCharacterTile = $(this).detach();
             $("#my-character").append(myCharacterTile);
-            $(this).css("background", "blue");
+            // $(this).css("background", "blue");
 
             // Add the associated character to the gameboard
             var charIndex = $(this).attr('id');
@@ -66,7 +99,7 @@ $(document).ready(function() {
             
             // Move the rest of the buttons to the enemies list
             $(".character-list>.tile").detach().appendTo(".enemy-list");
-            $(".enemy-list>.tile").css("background", "red");
+            // $(".enemy-list>.tile").css("background", "red");
             isPlayerSelected = true;
         } 
     });
@@ -81,12 +114,19 @@ $(document).ready(function() {
 
             isEnemySelected = true;
         }
-    })
+    });
 
     $("#attack-button").on("click", function() {
-        console.log("My character's name is: " + myCharacter.name);
-        console.log("My enemy's name is: " + myEnemy.name);
-    })
+        // console.log("My character's name is: " + myCharacter.name);
+        // console.log("My enemy's name is: " + myEnemy.name);
+
+        // Add myCharacter attackPts to the currentAttackPts
+        currentAttackPts += myCharacter.attackPts;
+        // console.log("New attack level: " + currentAttackPts);
+        // console.log("Counter attack: " + myEnemy.counterAttackPts);
+
+        evaluateAttack();
+    });
 
     
 
