4184e1f9e2276eb4ba6c32a5e7b3abcb69d7d448
Thomas Martin <tmartin.dev101010@gmail.com>
2018-08-21 17:28:48 -0600

    Majority of the code is working.  Starting the styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,6 +18,13 @@ var currentAttackPts = 0;
 var myCharacter = null;
 var myEnemy = null;
 
+// Array of character objects.
+var characters = [ new Character("Darth Vader", "./assets/images/Vader-1.jpg", 0, 125, 40, 20),
+                   new Character("Luke Skywalker", "./assets/images/luke-1.jpg", 1, 100, 60, 30),
+                   new Character("Boba Fett", "./assets/images/boba-1.jpg", 2, 80, 80, 40),
+                   new Character("Yoda", "./assets/images/yoda-1.jpg", 3, 200, 20, 10)];
+
+                   
 function buildCharacterTile(character) {
     var newCharacter = $("<button>");
     newCharacter.addClass("tile");
@@ -56,7 +63,10 @@ function evaluateAttack() {
         if ($(".enemy-list .tile").length > 0) {
             isEnemySelected = false;
         } else {
-            console.log("You won!");
+            $("#status").empty();
+            $("#status").append("<p>You are victorious...</p>");
+            $("#status").append("<p>Press the Reset button to play again!</p>");
+            generateReset();
         }
     } else {
         // Update the enemy hitpoint display
@@ -66,27 +76,36 @@ function evaluateAttack() {
         $("#status").append("<p>" + myEnemy.name + " has counter attacked with " + myEnemy.counterAttackPts + "!</p>");
 
         if (myCharacter.healthPts <= 0) {
-            console.log("Game Over.");
+            $("#status").empty();
+            $("#status").append("<p>You have been defeated...</p>");
+            $("#status").append("<p>Press the Reset button to play again!</p>");
+            generateReset();
         } else {
             $("#my-character #hitPoints").text(myCharacter.healthPts);
         }
     }
+}
 
+function generateReset() {
+    var newButton = $("<button>");
+    newButton.text("Reset Game");
+    newButton.addClass("reset");
+    $("#status").append(newButton);
+
+}
+
+function createCharacter() {
+    // Create a new character
+    for (let i = 0; i < characters.length; i++) {
+        buildCharacterTile(characters[i]);
+    }
 }
 
 $(document).ready(function() {
 
-    // Create a new character
-    var characters = [ new Character("Darth Vader", "./assets/images/Vader-1.jpg", 0, 125, 40, 20),
-                       new Character("Luke Skywalker", "./assets/images/luke-1.jpg", 1, 100, 60, 30),
-                       new Character("Boba Fett", "./assets/images/boba-1.jpg", 2, 80, 80, 40),
-                       new Character("Yoda", "./assets/images/yoda-1.jpg", 3, 200, 20, 10)];
-    
-    for (let i = 0; i < characters.length; i++) {
-        buildCharacterTile(characters[i]);
-    }
-    
-    $(".character-list>.tile").on("click", function() {
+    createCharacter();
+
+    $(".character-list").on("click", ".tile", function() {
         // If player isn't selected, move the clicked charcter to myCharacter
         if (!isPlayerSelected) {
             var myCharacterTile = $(this).detach();
@@ -117,15 +136,37 @@ $(document).ready(function() {
     });
 
     $("#attack-button").on("click", function() {
-        // console.log("My character's name is: " + myCharacter.name);
-        // console.log("My enemy's name is: " + myEnemy.name);
+                
+        if ($("#my-character .tile").length === 0) {
+            $("#status").empty();
+            $("#status").append("<p>Please select a character.</p>");
+        } else if ($(".defender .tile").length === 0) {
+            $("#status").empty();
+            $("#status").append("<p>Please select an opponent.</p>");
+        } else {
+            // Add myCharacter attackPts to the currentAttackPts
+            currentAttackPts += myCharacter.attackPts;
+            evaluateAttack();
+        }
 
-        // Add myCharacter attackPts to the currentAttackPts
-        currentAttackPts += myCharacter.attackPts;
-        // console.log("New attack level: " + currentAttackPts);
-        // console.log("Counter attack: " + myEnemy.counterAttackPts);
+    });
 
-        evaluateAttack();
+    // Event handler for reset button
+    $("#status").on("click", ".reset", function(){
+        // clear global variables
+        myCharacter = null;
+        myEnemy = null;
+        currentAttackPts = 0;
+        isEnemySelected = false;
+        isPlayerSelected = false;
+
+        // Empty divs
+        $("#my-character").empty();
+        $(".enemy-list").empty();
+        $("#status").empty();
+
+        // Create the characters again
+        createCharacter();
     });
 
     
